3c74271d091437a59c7f9b76588c12e6851e9d81
JVelasco-OBFV <velasco.jp@obmontessori.edu.ph>
2026-09-03 22:59:14 +0800

    Updated main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ heightincm_172 = 172.72 #floating-number
 fav_country101_ = ["Malaysia","Spain","Italy"] #list
 student_type = False #boolean
 descriptions = {
-    "color":"red",
+    "color":"Red",
     "car_brand":"Toyota",
     "shoe_size":6.8,
     "best_friend":"Wenz Paragas"
@@ -17,9 +17,9 @@ days_of_the_week = ("Sunday","Monday","Tuesday","Wednesday","Thursday","Friday",
 display(f'Name: {name}', target ="display")
 display(f'Age: {age}', target ="display")
 display(f'Height: {heightincm_172} cm', target="display")
-display(f'Countries I want to travel to: {fav_country101_[0], fav_country101_[1], fav_country101_[2]}', target="display")
+display(f'Countries I want to travel to: ', f'{fav_country101_[0]},' + f' {fav_country101_[1]},' + f' and {fav_country101_[2]}', target="display")
 display(f'Student Type: {student_type}', target="display")
-display(f'Favorite Color, Car Brand, Shoe Size, and Best Friend: ',  descriptions["color"], descriptions["car_brand"], descriptions["shoe_size"], descriptions["best_friend"], target="display")
+display(f'Favorite Color, Car Brand, Shoe Size, and Best Friend: ',  f'{descriptions["color"]},' + f' {descriptions["car_brand"]}'+ f' and { descriptions["shoe_size"]}', descriptions["best_friend"], target="display")
 display(f'Favorite Fruits: {my_fav_fruits}', target="display")
 display(f'Ranking Days of the Week: {days_of_the_week[0]} - 3rd Place, {days_of_the_week[1]} - 5th Place, {days_of_the_week[2]} - 6th Place, {days_of_the_week[3]} - 7th Place, {days_of_the_week[4]} - 4th Place, {days_of_the_week[5]} - 2nd Place, {days_of_the_week[6]} - 1st Place', target="display")
 
@@ -56,7 +56,7 @@ def floor_divide(e):
     num1= float(document.getElementById("input1").value)
     num2= float(document.getElementById("input2").value)
     result= num1 // num2
-    display(f'Your quotient (rounded rown) is equal to {result}.', target="output")
+    display(f'Your quotient (rounded down) is equal to {result}.', target="output")
 
 def modulo(e):
     document.getElementById("output").innerHTML = ""
